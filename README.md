275043f4395b90be230680d3a57e1cc35c525cf2
Joy Yeh <joyyeh.tw@gmail.com>
2019-12-30 13:14:44 +0800

    README and script setup

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
 # Enconv
 A light wrapper around chardet to conveniently convert text files.
 
+# Install
+
+Optionally create virtual env
+
 ```
+~$ pip install enconv
+```
+
+# Usage
+
+```bash
+~$ enconv
 usage: enconv.py [-h] [-ie INPUT_ENCODING] [-gl GUESS_LENGTH]
                  [-oe OUTPUT_ENCODING] [-of OUTPUT_FILE] [-ow]
                  input_file
+```
+
+# Example
+
+```bash
+~$ enconv /Users/me/Downloads/chinese.txt
+Guess: {'encoding': 'GB2312', 'confidence': 0.99, 'language': 'Chinese'}
+Wrote to /Users/me/Downloads/chinese.txt.utf-8 in utf-8
 ```
--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
     name="enconv-joy13975",
     version="0.0.1",
@@ -13,10 +16,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/joy13975/enconv",
     packages=setuptools.find_packages(),
+    scripts=['bin/enconv'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=required,
 )
