3fda467bb63a1e8bf7fef9af5abae5f171115f04
Joy Yeh <joyyeh.tw@gmail.com>
2019-12-30 12:04:38 +0800

    Initial add

---
+++ encoding_converter.py
@@ -0,0 +1,68 @@
+import chardet
+import os
+
+class EncodingConverter:
+    def __init__(self, *args, **kwargs):
+        """Constructor
+
+        :attribute str input_file: mandatory input file path
+        :attribute str input_encoding: optional input encoding
+        :attribute int guess_length: number of bytes to use for guessing
+        :attribute str output_encoding: optional output encoding
+        :attribute str output_file: optional output file path
+        :attribute bool overwrite: won't confirm overwrite when True
+        """
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+        assert getattr(self, 'input_file') is not None
+        self.filebytes = self._read()
+        assert getattr(self, 'input_encoding') is not None
+        assert getattr(self, 'guess_length') is not None
+        assert getattr(self, 'output_encoding') is not None
+        assert getattr(self, 'output_file') is not None
+        assert getattr(self, 'overwrite') is not None
+        self.converted_str = None
+
+    @property
+    def output_file(self):
+        file_path = self._output_file
+        # Create output file path if not specified
+        if file_path == '':
+            file_path = f'{self.input_file}.{self.output_encoding}'
+        # Create output file path if output is specified as directory
+        if os.path.isdir(file_path):
+            input_file_name = os.path.basename(self.input_file)
+            file_path = os.path.join(file_path, input_file_name)
+        return file_path
+    
+    @output_file.setter
+    def output_file(self, value):
+        self._output_file = value
+
+    def guess(self):
+        return chardet.detect(self.filebytes[:self.guess_length])
+
+    def convert(self):
+        try:
+            # Confirm overwrite
+            if os.path.exists(self.output_file) and not self.overwrite:
+                overwrite_ok = input('Output file exists. Overwrite?\n(y/n): ')
+                if overwrite_ok.lower() != 'y':
+                    print('Abort writting.')
+                    return
+            decoded_str = self.filebytes.decode(self.input_encoding, errors='ignore')
+            with open(self.output_file, 'w', encoding=self.output_encoding) as file:
+                file.write(decoded_str)
+        except Exception as e:
+            raise Exception(f'Failed to write to file {self.output_file}. ') from e
+
+
+    def _read(self):
+        try:
+            with open(self.input_file, 'rb') as file:
+                filebytes = b''
+                for line in file.readlines():
+                    filebytes += line
+                return filebytes
+        except Exception as e:
+            raise Exception(f'Failed to read file {self.input_file}. ') from e
---
+++ enconv.py
@@ -0,0 +1,37 @@
+import argparse
+
+from encoding_converter import EncodingConverter
+
+def main(*args, **kwargs):
+    ap = argparse.ArgumentParser(description='Guess and convert between encodings')
+    ap.add_argument('input_file',
+                    help='Input file path',
+                    type=str)
+    ap.add_argument('-ie', '--input_encoding',
+                    help='Input enconding',
+                    default='guess')
+    ap.add_argument('-gl', '--guess_length',
+                    help='Number of bytes to use for guessing.-1=all',
+                    default=1024)
+    ap.add_argument('-oe', '--output_encoding',
+                    help='Output enconding',
+                    default='utf-8')
+    ap.add_argument('-of', '--output_file',
+                    help='Output file path',
+                    default='')
+    ap.add_argument('-ow', '--overwrite', 
+                    help='When set, won\'t confirm when attempting to overwrite.',
+                    action='store_true')
+    args = ap.parse_args()
+    ec = EncodingConverter(**vars(args))
+    if ec.input_encoding.lower() == 'guess':
+        guess_result = ec.guess()
+        print(f'Guess: {guess_result}')
+        ec.input_encoding = guess_result['encoding']
+    ec.convert()
+    print(f'Wrote to {ec.output_file} in {ec.output_encoding}')
+    print('Done')
+
+
+if __name__  == '__main__':
+    main()
